916009e5057c86a4ab1b61144994da20bd523fb9
VickyBondarenko <quikyv@gmail.com>
2022-10-09 18:04:30 +0200

    Added css footer

--- src/App.js
+++ src/App.js
@@ -9,21 +9,23 @@ import Date from "./Date";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-5">
-          <Form />
-          <CityName />
-          <Description />
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5">
+            <Form />
+            <CityName />
+            <Description />
+          </div>
+          <div className="col">
+            <Temperature />
+            <Date />
+          </div>
+          <div className="col">
+            <img src={Icon} alt="sun" className="icon-today" />
+          </div>
         </div>
-        <div className="col">
-          <Temperature />
-          <Date />
-        </div>
-        <div className="col">
-          <img src={Icon} alt="sun" className="icon-today" />
-        </div>
-      </div>
+      </div>{" "}
       <small>
         <span class="target">
           <a
